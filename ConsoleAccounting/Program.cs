9e5f19b3ee83b393d33da93d9c4268edd1f144b5
Astralite <binarysproduct@gmail.com>
2020-10-27 16:25:18 +0800

    Create Staff Class

--- ConsoleAccounting/Program.cs
+++ ConsoleAccounting/Program.cs
@@ -10,12 +10,68 @@ namespace ConsoleAccounting
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            /*
+            Staff staff1 = new Staff("Kyle", 32.00f);
+            staff1.HoursWorked = 10;
+            staff1.CalculatePay();
+            Console.WriteLine(staff1.TotalPay);
+            Console.WriteLine(staff1.ToString());
+            */
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.ReadKey();
         }
     }
+
+    class Staff
+    {
+        private float hourlyRate;
+        private int hWorked;
+
+        public float TotalPay { get; protected set; }
+        public float BasicPay { get; private set; }
+        public string NameOfStaff { get; private set; }
+
+        public int HoursWorked
+        {
+            get
+            {
+                return hWorked;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    hWorked = value;
+                }
+                else
+                {
+                    hWorked = 0;
+                }
+            }
+        }
+
+        public Staff(string name, float rate)
+        {
+            NameOfStaff = name;
+            hourlyRate = rate;
+        }
+        
+        public virtual void CalculatePay()
+        {
+            Console.WriteLine("Calculating Pay...");
+            BasicPay = hWorked * hourlyRate;
+            TotalPay = BasicPay;
+        }
+
+        public override string ToString()
+        {
+            return "hourlyRate = " + hourlyRate + " hWorked = " + hWorked + " TotalPay = " + TotalPay + " BasicPay = " + BasicPay + " NameOfStaff = " + NameOfStaff;
+        }
+
+    }
+
+    // class Manager : Staff { }
+    // class Admin : Staff { }
+    // class FileReader { }
+    // class PaySlip { }
 }
